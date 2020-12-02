002ded806b20ca3b9324a0855b389bafceb1a000
Tharnid <jeffmsmithdev@gmail.com>
2020-12-01 23:07:57 -0500

    explored templates

--- dj3/password_generator_project/generator/views.py
+++ dj3/password_generator_project/generator/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    # return HttpResponse('Hello there friend!!!')
+    return render(request, 'generator/home.html', {'password': 'asdfasd223asdfas'})
+
+def eggs(request):
+    return HttpResponse('Eggs are Awesome!!!')
