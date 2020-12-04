c924506d5a6717b3d02f286a8d34254556b3e80e
Tharnid <jeffmsmithdev@gmail.com>
2020-12-04 14:03:21 -0500

    generates random password

--- dj3/password_generator_project/generator/views.py
+++ dj3/password_generator_project/generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
@@ -8,4 +9,14 @@ def home(request):
     return render(request, 'generator/home.html', {'password': 'asdfasd223asdfas'})
 
 def password(request):
-    return render(request, 'generator/password.html')
+    
+    thepassword = ''
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    length = 10
+
+    for x in range(length):
+        thepassword += random.choice(characters)
+    
+    return render(request, 'generator/password.html', {'password':thepassword})
