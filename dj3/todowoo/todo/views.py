e38bafba169cd29b02ccc70d22266c9a8c5f5303
Tharnid <jeffmsmithdev@gmail.com>
2021-07-13 17:08:45 -0400

    created project and completed log in

---
+++ dj3/todowoo/todo/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+    else:
+        # create new user
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error': 'Username has already been taken!!!'})
+        else:
+            # tell user passwords didn't match
+           return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error': 'Passwords do not match!!!'})
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
---
+++ dj3/todowoo/todowoo/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from todo import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Auth
+    path('signup/', views.signupuser, name='signupuser'),
+
+    # Todos
+    path('current/', views.currenttodos, name='currenttodos'),
+
+]
