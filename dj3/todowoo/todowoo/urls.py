412d3d03a94665b4ca4367f6abaeea1d47894bfe
Tharnid <jeffmsmithdev@gmail.com>
2021-07-16 00:50:35 -0400

    user auth completed

--- dj3/todowoo/todowoo/urls.py
+++ dj3/todowoo/todowoo/urls.py
@@ -7,8 +7,11 @@ urlpatterns = [
 
     # Auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
 
     # Todos
+     path('', views.home, name='home'),
     path('current/', views.currenttodos, name='currenttodos'),
 
 ]
